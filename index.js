b2285d24f55d76433b612b91dcbc0b3cd6266cf7
Victor Arnedo Blanco <victor.arnedo.93@gmail.com>
2022-01-07 21:21:37 +0100

    feat: add Fnac scraper

--- index.js
+++ index.js
@@ -11,9 +11,17 @@ const shops = [
       return content.includes('Agotado temporalmente') == false;
     },
   },
+  {
+    vendor: 'Fnac',
+    url: 'https://www.fnac.es/Consola-PlayStation-5-Videoconsola-Consola/a7724798',
+    checkStock: async ({ page }) => {
+      const content = await page.$$('.f-buyBox-availabilityStatus-unavailable');
+      return content.length == 0;
+    },
+  },
 ];
 (async () => {
-  const browser = await chromium.launch({ headless: false, slowMo: 50 });
+  const browser = await chromium.launch({ headless: false });
 
   for (const shop of shops) {
     const { vendor, url, checkStock } = shop;
@@ -25,6 +33,7 @@ const shops = [
 
     console.log(`${vendor}: ${hasStock ? 'HAS STOCK!!!' : 'Out of stock'}`);
     await page.screenshot({ path: `${vendor}.png` });
+    await page.close();
   }
   await browser.close();
 })();
